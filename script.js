f539d9bc08012e28f964b10cb9d3559e3cdf3ace
AlexBertuzzi <alexmichaelbertuzzi@yahoo.com>
2020-11-14 09:35:41 -0600

    getting the individual elements to function

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
-
-
-    
-var today = $("<p>");
+var today = $("p");
     today = moment().format("dddd, MMMM Do YYYY");
     $("#currentDay").append(today);
     
 var currentHour = moment().hour();
-    currentHour = moment(currentHour).format("hA");
+    currentHour = moment(currentHour).format("H");
+    currentHour = parseInt(currentHour)
     console.log(currentHour);
 
 $(".row").addClass("time-block");
@@ -14,12 +12,42 @@ $("h2").addClass("col-1 hour");
 $("textarea").addClass("col-10 description");
 $("button").addClass("col-1 saveBtn saveBtn i:hover");  
 
-        
-//var timeStamp = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"];
+function conditionalFormat(){ ///we need to break this down to work on a single element no loop if 9 =<> curent hour set class then repeat for every element
+    var dataHour = []
+        dataHour = $.makeArray($("textarea").map(function(){
+        return parseInt($(this).attr("data-hour"))
+         }));
+        console.log(dataHour)
+        for (var i = 0; i < dataHour.length; i++){
+            if (dataHour[i] < currentHour){
+                $("textarea").addClass("past")
+                $("textarea").removeClass("present future")
+            }
+            if (dataHour[i] == currentHour){
+                $("textarea").addClass("present")
+                $("textarea").removeClass("present future")
+            }
+            if (dataHour[i] > currentHour){
+                $("textarea").addClass("future")
+                $("textarea").removeClass("past present")
+            }
+        }
+};
+conditionalFormat();
 
-        
-        //var timeStamp = moment().hour(i);
-        //var timeStampText = moment(timeStamp).format("hA");
+$("#b9").on("click", function(event){ /// change all data to ids
+    event.preventDefault();
+    var textInput = {
+        text: document.querySelector("#t9").value.trim()
+    }
+    localStorage.setItem("textInput", textInput)
+    console.log(textInput)
+})
+
+
+
+//setInterval(conditionalFormat(), 5000);
+
 
        
         
